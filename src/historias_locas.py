9b6662bbea89bae8846a544fb732bc0296b09c36
Victor Lopez <56142145+andresxk@users.noreply.github.com>
2023-06-12 14:07:29 -0500

    se actualiza la infomracion de los archivos.

---
+++ src/historias_locas.py
@@ -0,0 +1,16 @@
+# Concatenar cadenas de caracteres.
+# Supongamos que queremos contatenar una cadena que diga:
+# Aprende a programamr con _______.
+# organizacion = "freeCodeCamp"
+# print(f"Aprende a programar con {organizacion}")
+
+# Madlib (Historias locas)
+
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo plural: ")
+
+madlib = f"¡Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. ¡Aprende a {verbo2} y alcanza tus {sustantivo_plural}!."
+
+print(madlib)
--- src/hola.py
+++
@@ -1,7 +0,0 @@
-# Definir funcion para cambiar nombre a capitalize
-def nombre_propio():
-    nombre = input("Ingrese su nombre: ")
-    return nombre.isupper()
-
-resultado = nombre_propio()
-print(resultado)
